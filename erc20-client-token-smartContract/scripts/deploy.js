c187035de3ce97f302bb9b34fee5fb2dcdbe56be
Vivekjoshi11 <108278744+Vivekjoshi11@users.noreply.github.com>
2025-06-20 13:32:42 +0530

    hardhat

--- erc20-client-token-smartContract/scripts/deploy.js
+++ erc20-client-token-smartContract/scripts/deploy.js
@@ -1,24 +1,3 @@
-// const hre = require("hardhat");
-
-// async function main() {
-//   const [deployer] = await hre.ethers.getSigners();
-
-//   console.log("Deploying contracts with account:", deployer.address);
-
-//   const Token = await hre.ethers.getContractFactory("RegisteredERC20");
-//   const token = await Token.deploy(deployer.address); // Pass initial owner
-
-//   await token.deployed();
-
-//   console.log("Contract deployed to:", token.address);
-// }
-
-// main().catch((error) => {
-//   console.error(error);
-//   process.exitCode = 1;
-// });
-
-
 const hre = require("hardhat");
 
 async function main() {
